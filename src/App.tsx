6e82ab5213b093d4aaf950fc50625b44fa8a16f4
valentinamoharem <vmoharem@pop-os.localdomain>
2021-03-10 02:39:01 -0300

    removing comments

--- src/App.tsx
+++ src/App.tsx
@@ -27,18 +27,3 @@ function App() {
 }
 
 export default App;
-
-
-// TO-DO
-// setCity according to input value
-// trigger input value with 'other' option in select input menu
-// setCity according to text field input value
-// handle error for typed city in text field input
-// GET forecast using city value
-// handle error for GET request 
-// set forecast value globally if succesful response
-// update app state if necessary (TEST)
-// display output forecast data
-// set change logic using existence of forecast data
-// add functionality to Back button (resets global state for city and/or forecast, sets both to null)
-// TEST value reset for global app
